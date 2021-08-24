beb95729aef86b62fb3e432cd606deef49a261b2
chaselover <gkstmdwn234@gmail.com>
2021-08-24 23:58:07 +0900

    Feat: Make polls's index and detail view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,37 @@
+# from django import template
 from django.http import HttpResponse
+# from django.template import loader
+# 숏컷으로 render 사용. template에 context를 채워넣어 표현한 경과를 HttpResponse객체와 함꼐 돌려주는 구문. render
+from django.shortcuts import render,get_object_or_404
+from django.http import Http404
+
+from .models import Question
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     output = ', '.join([q.question_text for q in latest_question_list])
+#     return HttpResponse(output)
 
 def index(request):
-    return HttpResponse("What the wonderful hell.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html',context)
+
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question doex not exist")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html',{'question':question})
+
+def results(request, question_id):
+    response = f"I'm the results of question {question_id}"
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    return HttpResponse(f"You vote this Q {question_id}")
