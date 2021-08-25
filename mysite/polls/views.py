de6787dafdb1ab3ad92fb0c22620d1e60a313d6c
chaselover <gkstmdwn234@gmail.com>
2021-08-26 02:04:24 +0900

    Feat: Feature views-testcodes

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import render,get_object_or_404
 # from django.http import Http404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question,Choice
 
@@ -41,12 +42,14 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
