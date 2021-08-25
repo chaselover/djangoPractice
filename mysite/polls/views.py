491057a26949e84d82a709c73b89155029a1f0da
chaselover <gkstmdwn234@gmail.com>
2021-08-26 01:05:51 +0900

    Refactor: refact views and feature generic views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,11 +1,12 @@
 # from django import template
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.http.response import HttpResponseRedirect
 # from django.template import loader
 # 숏컷으로 render 사용. template에 context를 채워넣어 표현한 경과를 HttpResponse객체와 함꼐 돌려주는 구문. render
 from django.shortcuts import render,get_object_or_404
 # from django.http import Http404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question,Choice
 
@@ -14,26 +15,41 @@ from .models import Question,Choice
 #     output = ', '.join([q.question_text for q in latest_question_list])
 #     return HttpResponse(output)
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    # return HttpResponse(template.render(context, request))
-    return render(request, 'polls/index.html',context)
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     # template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     # return HttpResponse(template.render(context, request))
+#     return render(request, 'polls/index.html',context)
 
-def detail(request, question_id):
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question doex not exist")
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html',{'question':question})
+# def detail(request, question_id):
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404("Question doex not exist")
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html',{'question':question})
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
